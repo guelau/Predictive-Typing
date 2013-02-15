aec46c7aac19e1731b42ae631676bc6c67b99e71
Laurent Guédon <tendencies@free.fr>
2013-02-15 23:36:28 +0100

    Fix to use last jQuery lib (>= 1.8). Changing the cloning method

--- predictiveTyping.js
+++ predictiveTyping.js
@@ -23,9 +23,9 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *  
- * @autors	laurent Guedon <info@tild.com>
- * @date	2011-01-26 18:11:21
- * @version	1.0.1
+ * @autors    laurent Guedon <info@tild.com>
+ * @date    2011-01-26 18:11:21
+ * @version    1.0.1
  */
 ;(function($){
     "use strict";
@@ -38,6 +38,7 @@
         // Access to jQuery and DOM versions of element
         base.$el = $(el);
         base.el = el;
+        base.zIndex = 0;
         
         base.lblText = '';
         
@@ -49,67 +50,27 @@
             var $el = base.$el;
             base.opts = $.extend({},$.predictiveTyping.defaultOptions, options);
             
-            base.label = getLabel();
+            base.lblText = base.opts.label || $('label[for="' + base.$el.attr('id') + '"]').text() || base.$el.attr('title');
+            // hide old label element
+            $('label[for="' + base.$el.attr('id') + '"]').hide();
 
             
             // Get the textbox styles used to mimic the displayed text of the textbox
             // To have a nice effect, you need to style your ($el) input like this :
             // # input {border:1px solid #000; margin:3px; padding:5px;}
-            base.inputStyles = {
-                width : $el.css('width')||'200px',
-                height : $el.css('height')||'16px',
-
-                fontSize : $el.css('font-size')||'100%',
-                fontFamily : $el.css('font-family')||'inherit',
-                fontVariant : $el.css('font-variant')||'normal',
-                fontWeight : $el.css('font-weight')||'normal',
-                fontStyle : $el.css('font-style')||'normal',
-                color : $el.css('color')||'inherit',
-                
-                /* @todo
-                letterSpacing : $el.css('letter-spacing'),
-                lineHeight : $el.css('line-height'),
-                textDecoration : $el.css('text-decoration'),
-                textTransform : $el.css('text-transform'),*/
-                
-                borderTopWidth : $el.css('border-top-width')||'1px',
-                borderRightWidth : $el.css('border-right-width')||'1px',
-                borderBottomWidth : $el.css('border-bottom-width')||'1px',
-                borderLeftWidth : $el.css('border-left-width')||'1px',
-                borderBottomColor : $el.css('border-bottom-color')||'#D4D4D4',
-                borderBottomStyle : $el.css('border-bottom-style')||'solid',
-                borderLeftColor : $el.css('border-left-color')||'#D4D4D4',
-                borderLeftStyle : $el.css('border-left-style')||'solid',
-                borderRightColor : $el.css('border-right-color')||'#D4D4D4',
-                borderRightStyle : $el.css('border-right-style')||'solid',
-                borderTopColor : $el.css('border-top-color')||'#D4D4D4',
-                borderTopStyle : $el.css('border-top-style')||'solid',
-                
-                outlineColor : $el.css('outline-color')||'none',
-                outlineStyle : $el.css('outline-style')||'none',
-                outlineWidth : $el.css('outline-width')||'none',
-                
-                backgroundColor : $el.css('background-color')||'white',
-                backgroundImage : $el.css('background-image')||'none',
-                backgroundRepeat : $el.css('background-repeat')||'repeat',
-                backgroundAttachment : $el.css('background-attachment')||'scroll',
-                backgroundPosition : $el.css('background-position')||'0% 0%',
-
-                marginTop : $el.css('margin-top')||0,
-                marginRight : $el.css('margin-right')||0,
-                marginBottom : $el.css('margin-bottom')||0,
-                marginLeft : $el.css('margin-left')||0,
-                
-                paddingTop : $el.css('padding-top')||0,
-                paddingRight : $el.css('padding-right')||0,
-                paddingBottom : $el.css('padding-bottom')||0,
-                paddingLeft : $el.css('padding-left')||0
-            };
+            var cStyles;
+            if ( typeof base.el.currentStyle != 'undefined' ) {
+                cStyles = base.el.currentStyle;
+            } else {
+                cStyles = document.defaultView.getComputedStyle(base.el, null);
+            }
             
-            replaceInput();
+            replaceInput(cStyles);
         };
         
-        var replaceInput = function() {
+        var replaceInput = function(clonedStyles) {
+            // The jQuery team removing $.browser :-/
+            var isMoz = /mozilla/.test(navigator.userAgent.toLowerCase()) && !/webkit/.test(navigator.userAgent.toLowerCase());
             var $el = base.$el;
             
             base.position = $el.position(); // Get old Input position
@@ -117,64 +78,67 @@
             var contener = document.createElement("div");
             var fullWidth = $el.outerWidth( true );
             var fullHeight = $el.outerHeight( true );
-            $(contener).css({'z-index':base.opts.zIndex+1, 'display':'block', 'position':'relative', 'width':fullWidth, 'height':fullHeight});
+            $(contener).css({'display':'block', 'position':'relative', 'width':fullWidth, 'height':fullHeight});
             $(contener).addClass(base.opts.labelClass + '-contener');
             
             // Create and set the label input
             base.label = document.createElement("input");
             
-            
             base.$label = $(base.label);
             
             var labelClass = base.opts.labelClass || 'predictive-text';
-            base.$label.attr({'class':labelClass + '-label', 'disabled':'disabled', 'autocomplete':'off'});
-            
+            base.$label.attr({'type': 'text', 'class':labelClass + '-label', 'disabled':'disabled', 'autocomplete':'off'});
             base.$label.val(base.lblText);
+            for ( var cStyle in clonedStyles ) {
+                // Clone default and user defined css styles
+                if ( typeof cStyle == "string" && (!isMoz || (isMoz && cStyle != "cssText"))) {
+                    try {
+                        base.label.style[cStyle] = clonedStyles[cStyle];
+                        if ( cStyle == "zIndex" && clonedStyles[cStyle] != "auto") { // getcurrent zIndex
+                            base.zIndex = clonedStyles[cStyle];
+                        }
+                        if(isMoz){
+                            if ( cStyle == "font" ) {
+                				base.label.style.fontSize = clonedStyles.fontSize;
+                			}
+                			if ( cStyle == "border" ) {
+                				base.label.style.borderSize = clonedStyles.borderSize;
+                			}
+                        }
+                    } catch (e) {}
+                }
+            }
             
-            
-            base.$label.css(base.inputStyles);
+            $(contener).css('z-index', base.zIndex+1);
             base.$label.css(
                     {
-                    'z-index':base.opts.zIndex,
-                    //'z-index':'-1',
-                    'color':base.inputStyles.color,
+                    'z-index':base.zIndex,
                     'position':'relative',
                     'top':-fullHeight,
                     'left':0
                     });
 
             // Up input style
-            $el.css(base.inputStyles);
             $el.css({'background':'transparent', 'position':'relative',
                     'top':0,
-                    'left':0, 'z-index':base.opts.zIndex+2});
-            $el.attr({'disabled':'', 'autocomplete':'off'});
+                    'left':0, 'z-index':base.zIndex+2});
+            $el.attr({'autocomplete':'off'});
             $el.addClass(base.opts.labelClass);
             
             $el.replaceWith(contener);
             $(contener).append($el, base.label);
-            //
-            //$el.before(base.label);
             
             base.$el.focus(function(e) { onFocus(e); })
                .blur(function(e) { onBlur(e); })
                .keyup(function(e) { onKeyup(e); })
                .keydown(function(e) { onKeypress(e); });
-			
         };
-        
-        var getLabel = function()
-        {
-            base.lblText = base.opts.label || $('label[for="' + base.$el.attr('id') + '"]').text() || base.$el.attr('title');
-            // hide old label
-            $('label[for="' + base.$el.attr('id') + '"]').hide();
-        };
-        
         
         var onFocus = function()
         {
-            base.$label.css('color', base.opts.labelFocusColor);
+            base.$label.css({ 'opacity' : 0.5 });
         };
+        
         var onBlur = function()
         {
             if(base.$el.val() === '')
@@ -185,7 +149,7 @@
             {
                 base.$label.val('');
             }
-            base.$label.css('color', base.inputStyles.color);
+            base.$label.css({ 'opacity' : 1 });
         };
         
         var onKeypress = function(e)
@@ -222,13 +186,13 @@
             if('' === srch) {base.$label.val(base.lblText); return true;}
             var exp = new RegExp('^(' + srch + ')', "i");
             base.$label.val('');
-			
-			
-			
-			if(typeof base.opts.source === 'function') {
-				var re = base.opts.source( srch );
-				base.$label.val( re.replace(exp, srch) );
-			}
+            
+            
+            
+            if(typeof base.opts.source === 'function') {
+                var re = base.opts.source( srch );
+                base.$label.val( re.replace(exp, srch) );
+            }
             else {
                 $(base.opts.source).each(function(k,v) { 
                     if( exp.test(v) ){
@@ -250,7 +214,6 @@
         labelClass: 'predictive-text',
         speed:100,
         labelColor:'#333',
-        labelFocusColor:'#ccc',
         source:{},
         zIndex:1000,
         allowTabKey:true
